f33a30d3e85cfa830d0b2e8e54d648edf1af58d7
chesyrin <chesyrin@users.noreply.github.com>
2016-10-28 21:40:40 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,13 @@
 public static main void (String[] args) {
   //do something
   //okay
+  
+  //Printing out "Hello World" in java
+  public class HelloWorld
+  {
+    public static void main (String [] args)
+    {
+      System.out.println ("Hello World");
+    }
+  }
 }
